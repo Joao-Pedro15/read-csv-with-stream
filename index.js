626f6e2a18615605342d0cf06f4ad585b916878e
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-23 16:11:54 -0300

    chore: return to read with a readFile and format data response

--- index.js
+++ index.js
@@ -7,17 +7,7 @@ const filePath = path.join(__dirname, 'myFileCsv', 'file.csv')
 
 ;
 (async() => {
-  const buffer = await readFile(filePath)
-  const readableFile = new Readable()
-  readableFile.push(buffer)
-  readableFile.push(null)
-
-  const test = readline.createInterface({
-    input: readableFile
-  })
-
-  for await(let tes of test){
-    console.log(tes);
-  }
+  const [header, ...rest] = (await readFile(filePath)).toString('utf8').split(/\r?\n/)
+  console.log(rest);
 
 })()
